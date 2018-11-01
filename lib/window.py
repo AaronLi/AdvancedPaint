d96cc3d0513646a74531cd455b9b3621e49203ff
Dumfing <dumpling.d.m@gmail.com>
2018-10-31 23:26:31 -0400

    rewrote resizing and drag and drop code to improve function and hopefully improve readability

--- lib/window.py
+++ lib/window.py
@@ -26,7 +26,6 @@ class Window:
         self.growButtonRect = Rect(width - 122, 0, 60, 20)
         self.minimizeButtonRect = Rect(width - 183, 0, 60, 20)
         self.lastButtonState = [False, False, False]
-        self.difX, self.difY = 0, 0
         self.minimized = False
         self.maximized = False
         self.closed = False
@@ -39,6 +38,7 @@ class Window:
         self.resizable = True
         self.program = program(self.getDrawArea(), self, programParameters)
         self.program.configure()
+        self.drag_offset_x, self.drag_offset_y = 0, 0
 
     def draw(self, surface: Surface, mousePos, mouseButtons, focused, availableSpace):
         normalisedMousePos = (mousePos[0] - self.position[0], mousePos[1] - self.position[1])
@@ -108,98 +108,66 @@ class Window:
     def update(self, mousePos: tuple, mouseButtons: tuple, availableSpace: Rect, focused: bool, interactable=True):
         resizeReady = [False, False, False, False]
         canvasMousePos = (mousePos[0] - self.position[0], mousePos[1] - self.position[1])
-        if not self.minimized and interactable:
-            # Dragging logic and snapping logic
-            if ((Rect(self.position[0], self.position[1] + 4, self.getWindowWidth() - 183, 16).collidepoint(
-                    mousePos) and not self.beingDragged) or (
-                        Rect(self.position[0] - 100, self.position[1] - 100, self.getWindowWidth() - 133,
-                             200).collidepoint(
-                            mousePos) and self.beingDragged)) and mouseButtons[0] and focused and not any(
-                self.beingResized):
-                if not self.lastButtonState[0] and mouseButtons[0]:
-                    self.difX = self.position[0] - mousePos[0]
-                    self.difY = self.position[1] - mousePos[1]
-                self.beingDragged = True
-                if self.resizable:
+        if mouseButtons[0]:
+            if self.beingDragged:
+                self.position = [mousePos[0] - self.drag_offset_x, mousePos[1] - self.drag_offset_y]
+            elif not self.minimized and interactable:
+                # Dragging logic and snapping logic
+                if self.get_title_bar_rect().collidepoint(mousePos) and not self.beingDragged and focused and not any(self.beingResized):
+
+                    #the window can be dragged if the mouse is touching the title bar, if it's not already being dragged, if it's focused, and if it's not being resized
+
+                    self.set_being_dragged(mouseButtons[0], mousePos)
+                    if self.resizable:
+                        if self.maximized:
+                            self.unMaximize()
+                        elif mousePos[1] < availableSpace.top + 5:
+                            self.snap[Window.WINDOW_TOP] = True
+
+                        elif mousePos[0] < availableSpace.left + 5:
+                            self.snap[Window.WINDOW_LEFT] = True
+
+                        elif mousePos[0] > availableSpace.width - 5:
+                            self.snap[Window.WINDOW_RIGHT] = True
+                        else:
+                            self.snap = [False, False, False]
+                else:
+                    # Snap only occurs after dragging has stopped
+                    self.beingDragged = False
+                    if self.snap[Window.WINDOW_TOP] and self.lastButtonState[0] and not mouseButtons[0]:
+                        self.maximize(availableSpace)
+                        self.snap[Window.WINDOW_TOP] = False
+                    elif self.snap[Window.WINDOW_LEFT] and self.lastButtonState[0] and not mouseButtons[0]:
+                        self.maximize(availableSpace.inflate(availableSpace.width // -2, 0))
+                        self.position = [0, 0]
+                        self.snap[Window.WINDOW_LEFT] = False
+                    elif self.snap[Window.WINDOW_RIGHT] and self.lastButtonState[0] and not mouseButtons[0]:
+                        self.maximize(availableSpace.inflate(availableSpace.width // -2, 0))
+                        self.position = [availableSpace.width // 2, 0]
+                        self.snap[Window.WINDOW_RIGHT] = False
+                if focused and self.resizable:
+                    if not any(self.beingResized):
+                        for index, side in enumerate(self.get_hot_edges()):
+                            if side.collidepoint(mousePos):
+                                self.set_being_resized(True, index)
+                                break
+                    else:
+                        for index, being_resized in enumerate(self.beingResized):
+                            if being_resized == True:
+                                self.resize_to_mouse(mousePos, index)
+                # Window control buttons
+                if self.minimizeButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0]:
+                    self.minimize()
+                elif self.growButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0] and self.resizable:
                     if self.maximized:
                         self.unMaximize()
-                    elif mousePos[1] < availableSpace.top + 5:
-                        self.snap[Window.WINDOW_TOP] = True
-
-                    elif mousePos[0] < availableSpace.left + 5:
-                        self.snap[Window.WINDOW_LEFT] = True
-
-                    elif mousePos[0] > availableSpace.width - 5:
-                        self.snap[Window.WINDOW_RIGHT] = True
                     else:
-                        self.snap = [False, False, False]
-                self.position = [mousePos[0] + self.difX, mousePos[1] + self.difY]
-            else:
-                # Snap only occurs after dragging has stopped
-                self.beingDragged = False
-                if self.snap[Window.WINDOW_TOP] and self.lastButtonState[0] and not mouseButtons[0]:
-                    self.maximize(availableSpace)
-                    self.snap[Window.WINDOW_TOP] = False
-                elif self.snap[Window.WINDOW_LEFT] and self.lastButtonState[0] and not mouseButtons[0]:
-                    self.maximize(availableSpace.inflate(availableSpace.width // -2, 0))
-                    self.position = [0, 0]
-                    self.snap[Window.WINDOW_LEFT] = False
-                elif self.snap[Window.WINDOW_RIGHT] and self.lastButtonState[0] and not mouseButtons[0]:
-                    self.maximize(availableSpace.inflate(availableSpace.width // -2, 0))
-                    self.position = [availableSpace.width // 2, 0]
-                    self.snap[Window.WINDOW_RIGHT] = False
-            if focused and self.resizable:
-                if ((canvasMousePos[0] in range(-4, 3) and not self.beingResized[Window.WINDOW_LEFT]) or (
-                                canvasMousePos[0] in range(-9, 10) and self.beingResized[Window.WINDOW_LEFT])) and \
-                                canvasMousePos[1] in range(20, self.getWindowHeight()):
-                    resizeReady[Window.WINDOW_LEFT] = True
-                    if mouseButtons[0]:
-                        self.resize((self.getRight() - mousePos[0], self.getWindowHeight()))
-                        self.position = [mousePos[0], self.position[1]]
-                        self.beingResized[Window.WINDOW_LEFT] = True
-                else:
-                    self.beingResized[Window.WINDOW_LEFT] = False
-                if ((canvasMousePos[0] - self.getWindowWidth() in range(-4, 3) and not self.beingResized[
-                    Window.WINDOW_RIGHT]) or (
-                                    canvasMousePos[0] - self.getWindowWidth() in range(-12, 13) and self.beingResized[
-                            Window.WINDOW_RIGHT])) and canvasMousePos[1] in range(20, self.getWindowHeight()):
-                    resizeReady[Window.WINDOW_RIGHT] = True
-                    if mouseButtons[0]:
-                        self.beingResized[Window.WINDOW_RIGHT] = True
-                        self.resize((mousePos[0] - self.position[0], self.getWindowHeight()))
-                else:
-                    self.beingResized[Window.WINDOW_RIGHT] = False
-                if canvasMousePos[0] in range(3, self.getWindowWidth() - 2) and ((canvasMousePos[
-                                                                                      1] - self.getWindowHeight() in range(
-                    -10, 0) and not self.beingResized[Window.WINDOW_BOTTOM]) or (
-                                    canvasMousePos[1] - self.getWindowHeight() in range(-7, 8) and self.beingResized[
-                            Window.WINDOW_BOTTOM])):
-                    resizeReady[Window.WINDOW_BOTTOM] = True
-                    if mouseButtons[0]:
-                        self.beingResized[Window.WINDOW_BOTTOM] = True
-                        self.resize((self.getWindowWidth(), mousePos[1] - self.position[1]))
-                else:
-                    self.beingResized[Window.WINDOW_BOTTOM] = False
-                if canvasMousePos[0] in range(0, self.getWindowWidth() - 183) and (
-                    (canvasMousePos[1] in range(-10, 5) and not self.beingResized[Window.WINDOW_TOP]) or (
-                        canvasMousePos[1] in range(-15, 13) and self.beingResized[Window.WINDOW_TOP])):
-                    resizeReady[Window.WINDOW_TOP] = True
-                    if mouseButtons[0]:
-                        self.resize((self.getWindowWidth(), self.getBottom() - mousePos[1]))
-                        self.position = [self.position[0], mousePos[1]]
-                        self.beingResized[Window.WINDOW_TOP] = True
-                else:
-                    self.beingResized[Window.WINDOW_TOP] = False
-            # Window control buttons
-            if self.minimizeButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0]:
-                self.minimize()
-            elif self.growButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0] and self.resizable:
-                if self.maximized:
-                    self.unMaximize()
-                else:
-                    self.maximize(availableSpace)
-            elif self.closeButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0]:
-                self.close()
+                        self.maximize(availableSpace)
+                elif self.closeButtonRect.collidepoint(canvasMousePos) and mouseButtons[0] and not self.lastButtonState[0]:
+                    self.close()
+        else:
+            self.set_being_dragged(False)
+            self.set_being_resized(False)
         if focused and not self.minimized:
             try:
                 self.program.update(self.programMousePos(mousePos), mouseButtons)
@@ -229,7 +197,7 @@ class Window:
         self.maximized = False
 
     def resize(self, newSize):
-        newSize = [max(newSize[0], 185), max(newSize[1], 25)]
+        newSize = [max(newSize[0], self.closeButtonRect.right - self.minimizeButtonRect.left +4), max(newSize[1], 25)]
         print("Window: \"%s\" resized to (%d, %d)" % (self.windowTitle, newSize[0], newSize[1]))
         self.windowCanvas = Surface(newSize)
         self.closeButtonRect = Rect(self.getWindowWidth() - 61, 0, 60, 20)
@@ -283,3 +251,60 @@ class Window:
 
     def collideWindow(self, point):
         return Rect(self.position, self.windowCanvas.get_size()).collidepoint(point)
+
+    def get_usable_title_bar_space(self):
+        #the amount of space that remains after the buttons are filled in
+        return self.getWindowWidth() - self.minimizeButtonRect.left
+
+    def set_being_dragged(self, state :bool, mousePos = (0, 0)):
+        if state == True:
+            self.drag_offset_x = mousePos[0] - self.position[0]
+            self.drag_offset_y = mousePos[1] - self.position[1]
+            self.beingDragged = True
+        else:
+            self.beingDragged = False
+
+    def set_being_resized(self, state :bool, side = -1):
+        if state == True:
+            if side > -1:
+                self.beingResized[side] = state
+            else:
+                raise ValueError("Invalid side %d"%side)
+        else:
+            for i in range(len(self.beingResized)):
+                self.beingResized[i] = False
+
+    def get_title_bar_rect(self):
+        #the box is moved 4 pixels down to make space for the dragging area
+        #the box is shrunk 183 pixels in width to make space for the close, minimize, and expand buttons
+        return Rect(self.position[0], self.position[1] + 4, self.getWindowWidth(), 16)
+
+    def get_hot_edges(self):
+        title_bar_rect = self.get_title_bar_rect()
+        return [
+            Rect(self.position[0], self.position[1]-10, self.get_usable_title_bar_space(), 14), #top
+            Rect(self.position[0]-4, title_bar_rect.bottom, 7, self.getWindowHeight()-(title_bar_rect.bottom - self.position[1])), #left
+            Rect(self.position[0] + self.getWindowWidth() -3, title_bar_rect.bottom, 7, self.getWindowHeight() - (title_bar_rect.bottom - self.position[1])), #right
+            Rect(self.position[0], self.getWindowHeight()-3, self.getWindowWidth(), 7) #bottom
+        ]
+
+    def resize_to_mouse(self, mousePos :tuple, side :int):
+
+        if side == Window.WINDOW_TOP:
+            self.resize((self.getWindowWidth(), self.getBottom()-mousePos[1]))
+            self.position = [self.position[0], mousePos[1]]
+
+        elif side == Window.WINDOW_LEFT:
+            self.resize((self.getRight() - mousePos[0], self.getWindowHeight()))
+            self.position = [mousePos[0], self.position[1]]
+
+        elif side == Window.WINDOW_RIGHT:
+            self.resize((mousePos[0] - self.position[0], self.getWindowHeight()))
+            self.position = self.position #unchanged
+
+        elif side == Window.WINDOW_BOTTOM:
+            self.resize((self.getWindowWidth(), mousePos[1] - self.position[1]))
+            self.position = self.position #unchanged
+
+    def get_window_buttons_rect(self):
+        return Rect(self.minimizeButtonRect.left, self.minimizeButtonRect.top, self.closeButtonRect.right - self.minimizeButtonRect.left, self.minimizeButtonRect.height)
--- programs/canvas/canvas.py
+++ programs/canvas/canvas.py
@@ -1,12 +1,15 @@
 from pygame import Surface, draw, Rect
 import lib.windowedprogram
-from programs.canvas.lib import dropdown, dropdownoption
+from lib import dropdown, dropdownoption
+
+
 class Canvas(lib.windowedprogram.WindowedProgram):
     def configure(self):
         self.window.windowTitle = 'Canvas'
         self.window.scaleToDrawArea((800,626))
         self.window.resizable = False
-        self.layerData.addLayer(Rect(0,0,self.availableSpace[0], self.availableSpace[1]-26))
+        # support for multiple layers for drawing
+        self.layerData.addLayer(Rect(0,0,self.availableSpace[0], self.availableSpace[1]))
     def draw(self, mousePos, mouseButtons):
         drawSurf = Surface(self.availableSpace)
         self.layerData.draw(drawSurf.subsurface(Rect(0,26,self.availableSpace[0], self.availableSpace[1]-26)))
@@ -23,12 +26,12 @@ class Canvas(lib.windowedprogram.WindowedProgram):
         super().__init__(availableSpace, window)
         self.dropdowns = []
         self.toolboxData, self.layerData = additionalParams['toolboxData'], additionalParams['layerData']
-        fileDropDown = dropdown.Dropdown((0,0),label="File")
+        fileDropDown = dropdown.Dropdown((0, 0), label="File")
         fileDropDown.addButton(dropdownoption.DropDownOption("New"))
         fileDropDown.addButton(dropdownoption.DropDownOption("Save"))
         fileDropDown.addButton(dropdownoption.DropDownOption("Load"))
         self.dropdowns.append(fileDropDown)
-        editDropDown = dropdown.Dropdown((fileDropDown.getWidth()+1, 0), label="Edit")
+        editDropDown = dropdown.Dropdown((fileDropDown.getWidth() + 1, 0), label="Edit")
         editDropDown.addButton(dropdownoption.DropDownOption("Undo"))
         editDropDown.addButton(dropdownoption.DropDownOption("Redo"))
         self.dropdowns.append(editDropDown)
